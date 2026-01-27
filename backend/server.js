b8f5a0b71513c0b1da2e518afa3777061b1c955f
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-27 23:27:38 +0530

    Add nanoid dependency and implement URL shortening endpoint.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from "express";
 const app = express();
+import { nanoid } from "nanoid";
 
 const PORT = 3000;
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/create", (req, res) => {
+  console.log(req.body.url);
+  res.send(nanoid(7));
 });
 
 app.listen(PORT, () => {
