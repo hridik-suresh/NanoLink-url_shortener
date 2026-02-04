b78094d6a532f528a336daabac8e770ae74c834a
hridik-suresh <iamhridiksuresh@gmail.com>
2026-02-05 03:05:59 +0530

    Added the verifi-emain/:token route to the authRoutes.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,11 +4,13 @@ import {
   login,
   forgotPassword,
   resetPassword,
+  verifyEmail,
 } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.get("/verify-email/:token", verifyEmail);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
 router.patch("/reset-password/:token", resetPassword);
