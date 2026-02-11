098a5abfb3a8b4934b0ded3ef099cac3eae69832
hridik-suresh <iamhridiksuresh@gmail.com>
2026-02-11 22:47:41 +0530

    Added the route for deleting the URL and it's analytics.

--- backend/src/routes/urlRoutes.js
+++ backend/src/routes/urlRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createShortUrl,
   updateUrlAlias,
+  deleteUrl,
 } from "../controllers/urlController.js";
 import { protect, optionalProtect } from "../middleware/authMiddleware.js";
 
@@ -14,5 +15,9 @@ router.post("/create", optionalProtect, createShortUrl);
 // Update Alias - strictly for the link owner
 // PATCH /api/url/:id
 router.patch("/:id", protect, updateUrlAlias);
+
+// Delete URL - strictly for the link owner
+// DELETE /api/url/:id
+router.delete("/:id", protect, deleteUrl);
 
 export default router;
