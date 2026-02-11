024590be909b03c5c2bc1297c36c8066f74539de
hridik-suresh <iamhridiksuresh@gmail.com>
2026-02-11 22:37:39 +0530

    Added the route for getting single URL analytics.

--- backend/src/routes/analyticsRoutes.js
+++ backend/src/routes/analyticsRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getUserDashboardStats } from "../controllers/analyticsController.js";
+import {
+  getUserDashboardStats,
+  getSingleUrlAnalytics,
+} from "../controllers/analyticsController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // GET /api/analytics/dashboard
 router.get("/dashboard", protect, getUserDashboardStats);
+
+// GET /api/analytics/url/:id
+router.get("/url/:urlId", protect, getSingleUrlAnalytics);
 
 export default router;
