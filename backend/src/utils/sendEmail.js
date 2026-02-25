c9b4897593dd2e6b023d69b02ad3e8daa380f9b9
hridik-suresh <iamhridiksuresh@gmail.com>
2026-02-25 15:10:44 +0530

    feat: Solving the issue in sending the verification email. Render's Free Tier blocks all outbound traffic on standard email ports (25, 465, and 587) to prevent spam. So we change the port and send the email to "smtp.gmail.com" from there the email will be delivered to the user.

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -3,10 +3,17 @@ import nodemailer from "nodemailer";
 const sendEmail = async (options) => {
   // 1. "transporter" (The Postman)
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    // Using host instead of 'service' gives more control for production
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for port 465
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
+    },
+    // This helps bypass certificate issues common in cloud environments
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
@@ -16,6 +23,10 @@ const sendEmail = async (options) => {
     to: options.email,
     subject: options.subject,
     text: options.message,
+    html: `<div style="font-family: sans-serif; padding: 20px; border: 1px solid #eee;">
+            <h2>NanoLink Email Verification</h2>
+            <p>${options.message.replace(/\n/g, "<br>")}</p>
+           </div>`,
   };
 
   // 3. Sends the email
